8ed20536443a774210adb68f8bf18378ea6f4a98
Daniel Medeiros <daniel@arneam.com>
2020-05-24 09:00:18 -0300

    Improve code format and remove unused import

--- src/main/java/com/arneam/kafka/streams/covid19api/CountrySerde.java
+++ src/main/java/com/arneam/kafka/streams/covid19api/CountrySerde.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Map;
-import lombok.SneakyThrows;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serializer;
