25300241966c0fe207b084d6c680f8c4b2bebe4e
Daniel Medeiros <daniel@arneam.com>
2020-06-13 10:13:14 -0300

    Remove unused imports and format code

--- src/test/java/com/arneam/kafka/streams/covid19api/Covid19ConfigIT.java
+++ src/test/java/com/arneam/kafka/streams/covid19api/Covid19ConfigIT.java
@@ -3,11 +3,8 @@ package com.arneam.kafka.streams.covid19api;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 
 import java.util.Properties;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.streams.StreamsConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,8 +22,8 @@ class Covid19ConfigIT {
 
   @ParameterizedTest
   @CsvSource(delimiter = '=', value = {"application.id=covid19api-application-04",
-    "bootstrap.servers=localhost:9092", "auto.offset.reset=earliest",
-    "processing.guarantee=exactly_once", "input.topic=covid-input", "output.topic=covid-output"})
+      "bootstrap.servers=localhost:9092", "auto.offset.reset=earliest",
+      "processing.guarantee=exactly_once", "input.topic=covid-input", "output.topic=covid-output"})
   void shouldLoadPropertiesFile(String key, String value) {
     assertThat(config.getProperty(key), is(equalTo(value)));
   }
